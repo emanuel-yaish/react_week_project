85240a380d5feec63af27bcabbe58c3d9ef3b0cf
Emanuel Yaish <emanuelyaish@gmail.com>
2022-01-03 00:56:10 +0200

    personal info fix

--- src/pages/ProfilesPage.jsx
+++ src/pages/ProfilesPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Nav from "../components/Nav";
+// import Nav from "../components/Nav";
 import Profiles from "../components/Profiles";
 import "./ProfilesPage.css";
 
