6cd8a522ac60b870d568d385aeb7352495d4bdcd
Emanuel Yaish <emanuelyaish@gmail.com>
2022-01-05 10:16:03 +0200

    find nelify problem

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,82 +1,82 @@
-import React from "react";
-import CostumInput from "../components/CostumInput";
-import RadioButtons from "./RadioButtons";
-import { MultiSelect } from "react-multi-select-component";
-import TextArea from "../components/TextArea";
+// import React from "react";
+// import CostumInput from "../components/CostumInput";
+// import RadioButtons from "./RadioButtons";
+// import { MultiSelect } from "react-multi-select-component";
+// import TextArea from "../components/TextArea";
 
-function PersonalInfo({ userInput, handleInputChange, selected, setSelected }) {
-  const gender = ["Male", "Female"];
-  //   const status = ["Single", "Divorced"];
-  //   const yesNoQuestion = ["Yes", "No"];
-  const options = [
-    { label: "Grapes 🍇", value: "grapes" },
-    { label: "Mango 🥭", value: "mango" },
-    { label: "Strawberry 🍓", value: "strawberry", disabled: true },
-  ];
-  return (
-    <div className="personal-info">
-      <CostumInput
-        onInputChangeCallBack={handleInputChange}
-        label="Name"
-        type="text"
-        name="name"
-        value={userInput.name}
-        id="name"
-      />
-      <CostumInput
-        onInputChangeCallBack={handleInputChange}
-        label="Birthday"
-        type="date"
-        name="birthDay"
-        value={userInput.birthDay}
-        id="birthday-input"
-      />
-      <RadioButtons
-        onInputChangeCallBack={handleInputChange}
-        mainLabel="Gender"
-        options={gender}
-      />
-      <CostumInput
-        onInputChangeCallBack={handleInputChange}
-        label="Avatar"
-        type="text"
-        name="avatar"
-        value={userInput.avatar}
-        id="avatar-input"
-      />
-      <CostumInput
-        onInputChangeCallBack={handleInputChange}
-        label="Height"
-        type="number"
-        name="height"
-        value={userInput.height}
-        id="height-input"
-      />
-      <CostumInput
-        onInputChangeCallBack={handleInputChange}
-        label="Status"
-        type="text"
-        name="status"
-        value={userInput.status}
-        id="status-input"
-      />
-      <MultiSelect
-        options={options}
-        value={selected}
-        onChange={setSelected}
-        labelledBy="Select"
-      />
-      <TextArea
-        label="About"
-        name="about"
-        id="about-input"
-        value={userInput.about}
-        onInputChangeCallBack={handleInputChange}
-        cols={40}
-        rows={5}
-      />
-    </div>
-  );
-}
+// function PersonalInfo({ userInput, handleInputChange, selected, setSelected }) {
+//   const gender = ["Male", "Female"];
+//   //   const status = ["Single", "Divorced"];
+//   //   const yesNoQuestion = ["Yes", "No"];
+//   const options = [
+//     { label: "Grapes 🍇", value: "grapes" },
+//     { label: "Mango 🥭", value: "mango" },
+//     { label: "Strawberry 🍓", value: "strawberry", disabled: true },
+//   ];
+//   return (
+//     <div className="personal-info">
+//       <CostumInput
+//         onInputChangeCallBack={handleInputChange}
+//         label="Name"
+//         type="text"
+//         name="name"
+//         value={userInput.name}
+//         id="name"
+//       />
+//       <CostumInput
+//         onInputChangeCallBack={handleInputChange}
+//         label="Birthday"
+//         type="date"
+//         name="birthDay"
+//         value={userInput.birthDay}
+//         id="birthday-input"
+//       />
+//       <RadioButtons
+//         onInputChangeCallBack={handleInputChange}
+//         mainLabel="Gender"
+//         options={gender}
+//       />
+//       <CostumInput
+//         onInputChangeCallBack={handleInputChange}
+//         label="Avatar"
+//         type="text"
+//         name="avatar"
+//         value={userInput.avatar}
+//         id="avatar-input"
+//       />
+//       <CostumInput
+//         onInputChangeCallBack={handleInputChange}
+//         label="Height"
+//         type="number"
+//         name="height"
+//         value={userInput.height}
+//         id="height-input"
+//       />
+//       <CostumInput
+//         onInputChangeCallBack={handleInputChange}
+//         label="Status"
+//         type="text"
+//         name="status"
+//         value={userInput.status}
+//         id="status-input"
+//       />
+//       <MultiSelect
+//         options={options}
+//         value={selected}
+//         onChange={setSelected}
+//         labelledBy="Select"
+//       />
+//       <TextArea
+//         label="About"
+//         name="about"
+//         id="about-input"
+//         value={userInput.about}
+//         onInputChangeCallBack={handleInputChange}
+//         cols={40}
+//         rows={5}
+//       />
+//     </div>
+//   );
+// }
 
-export default PersonalInfo;
+// export default PersonalInfo;
